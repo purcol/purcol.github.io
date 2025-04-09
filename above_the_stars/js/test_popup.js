9d85d30fa5c3990242382b27dc6577a24d9e29ec
Слушатель школьный №8 <shl08@psta.ru>
2025-04-09 15:16:43 +0300

    adding test poly & some stye to the popups

--- above_the_stars/js/test_popup.js
+++ above_the_stars/js/test_popup.js
@@ -1,6 +1,11 @@
 const testPopup = new Popup({
     id: "test-popup",
     title: "My First Popup",
+    backgroundColor: "#040013ff",
+    borderWidth: "10px",
+    borderColor: "#fbf7ffff",
+    titleColor: "#fbf7ffff",
+    textColor: "#fbf7ffff",
     content: `
         An example popup.
         Можно использовать любое кол-во строк.`,
